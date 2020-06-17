8e53944be42eb2d4c10d4f38ca1dbbaf982e3b3e
Brittani S Avery <brittani.s.avery@gmail.com>
2020-06-17 00:12:18 -0500

    Finish search form layout

--- pages/search.js
+++ pages/search.js
@@ -11,10 +11,27 @@ import {
   Checkbox,
   FormLabel,
   Grid,
+  Button,
 } from "@material-ui/core";
-import { SEARCH_LINEAGES, HOLIDAYS, LINEAGE_TYPES } from "../lib/constants";
+import {
+  SEARCH_LINEAGES,
+  HOLIDAYS,
+  LINEAGE_TYPES,
+  LINEAGE_SUB_TYPES,
+} from "../lib/constants";
 import Autocomplete from "@material-ui/lab/Autocomplete";
+import { makeStyles } from "@material-ui/core/styles";
 import { getMaleBreeds, getFemaleBreeds } from "../lib/breeds";
+
+const useStyles = makeStyles((theme) => ({
+  submitButton: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+  generation: {
+    alignSelf: "flex-start",
+  },
+}));
 
 export default function Search() {
   const router = useRouter();
@@ -23,17 +40,19 @@ export default function Search() {
   const maleBreeds = getMaleBreeds();
   const femaleBreeds = getFemaleBreeds();
 
+  const classes = useStyles();
+
   return (
     <Layout title="Search">
       <Typography variant="h2" component="h1">
         {SEARCH_LINEAGES}
       </Typography>
-      <Typography variant="h6" component="p">
+      <Typography variant="h6" component="p" gutterBottom>
         Use the form below to search the database for lineages.
       </Typography>
       <form noValidate autoComplete="off">
-        <Grid container spacing={2}>
-          <Grid item xs sm>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={12} sm={12} md={5}>
             <Autocomplete
               fullwidth
               id="male-breed"
@@ -45,20 +64,27 @@ export default function Search() {
             />
           </Grid>
           <Grid item xs={12} sm={12} md={2}>
-            <FormControlLabel
-              control={
-                <Switch
-                  //checked={state.checkedB}
-                  //onChange={handleChange}
-                  id="conditional-search"
-                  name="conditional"
-                  color="primary"
-                />
-              }
-              label="AND/OR"
-            />
+            <Typography component="div" align="center">
+              <Grid component="label" container alignItems="center" spacing={1}>
+                <Grid item xs>
+                  And
+                </Grid>
+                <Grid item xs={6}>
+                  <Switch
+                    //checked={state.checkedB}
+                    //onChange={handleChange}
+                    id="conditional-search"
+                    name="conditional"
+                    color="primary"
+                  />
+                </Grid>
+                <Grid item xs>
+                  Or
+                </Grid>
+              </Grid>
+            </Typography>
           </Grid>
-          <Grid item xs sm>
+          <Grid item xs={12} sm={12} md={5}>
             <Autocomplete
               fullwidth
               id="female-breed"
@@ -73,33 +99,58 @@ export default function Search() {
               )}
             />
           </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <Autocomplete
+              id="type"
+              options={LINEAGE_TYPES}
+              renderInput={(params) => (
+                <TextField {...params} label="Type" variant="outlined" />
+              )}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <Autocomplete
+              id="subtype"
+              options={LINEAGE_SUB_TYPES}
+              renderInput={(params) => (
+                <TextField {...params} label="Subtype" variant="outlined" />
+              )}
+            />
+          </Grid>
+          <Grid item xs={4} md={2} className={classes.generation}>
+            <TextField
+              fullWidth
+              id="generation"
+              label="Generation"
+              type="number"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs={8} md={10}>
+            <FormControl component="fieldset">
+              <FormLabel component="legend">Holiday</FormLabel>
+              <FormGroup row>
+                {HOLIDAYS.map((holiday) => (
+                  <FormControlLabel
+                    key={holiday}
+                    control={<Checkbox name={holiday.toLowerCase()} />}
+                    label={holiday}
+                  />
+                ))}
+              </FormGroup>
+            </FormControl>
+          </Grid>
         </Grid>
-
-        <Autocomplete
-          id="type"
-          options={LINEAGE_TYPES}
-          renderInput={(params) => (
-            <TextField {...params} label="Type" variant="outlined" />
-          )}
-        />
-        <TextField
-          id="generation"
-          label="Generation"
-          type="number"
-          variant="outlined"
-        />
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Holiday</FormLabel>
-          <FormGroup row>
-            {HOLIDAYS.map((holiday) => (
-              <FormControlLabel
-                key={holiday}
-                control={<Checkbox name={holiday.toLowerCase()} />}
-                label={holiday}
-              />
-            ))}
-          </FormGroup>
-        </FormControl>
+        <Grid container justify="center">
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.submitButton}
+          >
+            Search
+          </Button>
+        </Grid>
       </form>
     </Layout>
   );
