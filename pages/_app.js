286ecd89e8eb75adf003504e325f58a07febf98a
Brittani S Avery <brittani.s.avery@gmail.com>
2020-07-28 22:24:27 -0500

    Add swr and start add lineage call

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import React from "react";
+import { SWRConfig } from "swr";
 import "../styles.scss";
 
+// eslint-disable-next-line react/prop-types
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SWRConfig
+      value={{ fetcher: (...args) => fetch(...args).then((res) => res.json()) }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
